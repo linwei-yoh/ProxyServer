911dc1fba092b5a9f356ac75e36247c24f88c2ad
linwei-yoh <395373035@qq.com>
2017-07-18 21:36:59 +0800

    update

---
+++ ProxySites/__init__.py
@@ -0,0 +1,12 @@
+#-*- coding: utf-8 -*-
+
+from CN_bugng import BugngSpider
+from CN_kuaidaili import KuaiDaiLiSpider
+from CN_sixsixip import SixSixipSpider
+from CN_xiciSpider import XiciSpider
+from CN_superfastip import SuperfastipSpider
+
+from EN_freeproxylists import FreeProxyListsSpider
+from EN_gatherproxy import GatherproxySpider
+from EN_proxylistplus import ProxylistplusSpider
+from EN_usproxy import UsProxySpider
